3dea6878af8be0a8c348907addaea022bb0839b5
Chris Hoffman <chris.hoffman@acquia.com>
2014-02-01 02:41:40 -0700

    http auth

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -227,6 +227,26 @@ $databases = array (
 );
 
 /**
+ * Access controls for php-fpm: https://docs.acquia.com/articles/password-protect-your-non-production-environments-acquia-hosting
+ */
+// Make sure Drush keeps working. 
+// Modified from function drush_verify_cli()
+$cli = (php_sapi_name() == 'cli');
+// PASSWORD-PROTECT NON-PRODUCTION SITES (i.e. staging/dev)
+if (!$cli && (isset($_ENV['AH_NON_PRODUCTION']) && $_ENV['AH_NON_PRODUCTION'])) {
+  $username = '[admin]';
+  $password = '[mm44dagYYY!]';
+  if (!(isset($_SERVER['PHP_AUTH_USER']) && ($_SERVER['PHP_AUTH_USER']==$username && $_SERVER['PHP_AUTH_PW']==$password))) {
+    header('WWW-Authenticate: Basic realm="This site is protected"');
+    header('HTTP/1.0 401 Unauthorized');
+    // Fallback message when the user presses cancel / escape
+    echo 'Access denied';
+    exit;
+  }
+}
+
+
+/**
  * Access control for update.php script.
  *
  * If you are updating your Drupal installation using the update.php script but
