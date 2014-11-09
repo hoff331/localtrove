45731fed18ef7439dafd5499a794af93565f1325
Chris Hoffman <chris.hoffman@acquia.com>
2014-11-09 12:37:00 -0800

    update basic auth creds

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -220,8 +220,8 @@ $cli = (php_sapi_name() == 'cli');
 // PASSWORD-PROTECT NON-PRODUCTION SITES (i.e. staging/dev)
 
 if (!$cli && (isset($_ENV['AH_NON_PRODUCTION']) && $_ENV['AH_NON_PRODUCTION'])) {
-  $username = 'admin';
-  $password = 'mm44dagYYY!';
+  $username = 'local';
+  $password = 'trove';
   if (!(isset($_SERVER['PHP_AUTH_USER']) && ($_SERVER['PHP_AUTH_USER']==$username && $_SERVER['PHP_AUTH_PW']==$password))) {
     header('WWW-Authenticate: Basic realm="This site is protected"');
     header('HTTP/1.0 401 Unauthorized');
