6d75e8cfffde1185de0ffd77955472df2303b8fa
Chris Hoffman <chris.hoffman@acquia.com>
2015-04-12 14:35:59 -0700

    css/js updates

--- docroot/sites/all/themes/ltv2/js/script.js
+++ docroot/sites/all/themes/ltv2/js/script.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
   // Code that uses jQuery's $ can follow here.
   $( "#user" ).click(function() {
-	  $( "#userMenu" ).toggleClass("hide");
+	  $( "#userMenu" ).toggle();
 	});
 });
