0bfb2a31afe008130a4169e1a768f5f608e8da4a
Chris Hoffman <chris.hoffman@acquia.com>
2015-04-12 14:33:09 -0700

    css updates

--- docroot/sites/all/themes/ltv2/js/scripts.js
+++ docroot/sites/all/themes/ltv2/js/scripts.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
   // Code that uses jQuery's $ can follow here.
   $( "#user" ).click(function() {
-	  $( "#userMenu" ).toggle();
+	  $( "#userMenu" ).toggleClass("hide");
 	});
 });
--- docroot/sites/all/themes/ltv2/templates/html.tpl.php
+++ docroot/sites/all/themes/ltv2/templates/html.tpl.php
@@ -1,6 +1,3 @@
-<?php
-
-?>
 <!DOCTYPE html>
 <html <?php print $rdf_namespaces; ?>>
 
