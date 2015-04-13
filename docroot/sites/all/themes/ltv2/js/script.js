8e5c6a0b129895e3ee96d0d286a01783a0ea1558
Chris Hoffman <chris.hoffman@acquia.com>
2015-04-12 20:15:28 -0700

    css updates

--- docroot/sites/all/themes/ltv2/js/script.js
+++ docroot/sites/all/themes/ltv2/js/script.js
@@ -1,7 +1,11 @@
 jQuery(document).ready(function($) {
   // Code that uses jQuery's $ can follow here.
-  $( "#user" ).click(function() {
-	  $( "#userMenu" ).toggle();
-	  $( "#user" ).toggleClass( "active");
+	$( "#user" ).click(function() {
+		$( "#userMenu" ).toggle();
+		$( "#user" ).toggleClass( "active");
+	});
+	
+	$( "#vendorRegistrationFlag" ).click(function() {
+		$( "#vendorRegistration" ).slideToggle();
 	});
 });
